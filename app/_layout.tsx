d3a2c21a303db4e794a836d9be58381efc08e558
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-12-10 00:59:37 +0300

    message

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,14 @@
+/* eslint-disable prettier/prettier */
 import '../global.css';
 
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{headerShown:false}} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
 }
+ 
