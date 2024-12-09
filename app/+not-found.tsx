3c5f7bb6dc03896813b7f55d17fdb013e1bc59f5
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-12-10 01:04:30 +0300

    message

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,24 +1,29 @@
-/* eslint-disable prettier/prettier */
-import { Link, Stack } from 'expo-router';
-import { Text } from 'react-native';
-
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-     
-        <Text className={styles.title}>This screen doesn't exist.</Text>
-        <Link href="/" className={styles.link}>
-          <Text className={styles.linkText}>Go to home screen!</Text>
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View style={styles.container}>
+        <Text>This screen doesn't exist.</Text>
+        <Link href="/" style={styles.link}>
+          <Text>Go to home screen!</Text>
         </Link>
-
+      </View>
     </>
   );
 }
 
-const styles = {
-  title: `text-xl font-bold`,
-  link: `mt-4 pt-4`,
-  linkText: `text-base text-[#2e78b7]`,
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
