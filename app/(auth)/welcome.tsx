cd2d4bce9c594c138924eb54878fb1a8205056d5
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-12-10 14:36:54 +0300

    message

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,41 +1,53 @@
-import { router } from 'expo-router';
-import { useRef, useState } from 'react';
-import { Image, Text, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import Swiper from 'react-native-swiper';
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
 
-// import CustomButton from '@/components/CustomButton';
-// import { onboarding } from '@/constants';
+import CustomButtonr from "~/components/CustomButton";
+import { onboarding } from "~/constants";
 
 const Home = () => {
   const swiperRef = useRef<Swiper>(null);
   const [activeIndex, setActiveIndex] = useState(0);
 
-//   const isLastSlide = activeIndex === onboarding.length - 1;
+  const isLastSlide = activeIndex === onboarding.length - 1;
 
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
         onPress={() => {
-          router.replace('/(auth)/sign-up');
+          router.replace("/(auth)/sign-up");
         }}
-        className="flex w-full items-end justify-end p-5">
-        <Text className="text-md font-JakartaBold text-black">Skip</Text>
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
 
       <Swiper
         ref={swiperRef}
         loop={false}
-        dot={<View className="mx-1 h-[4px] w-[32px] rounded-full bg-[#E2E8F0]" />}
-        activeDot={<View className="mx-1 h-[4px] w-[32px] rounded-full bg-[#0286FF]" />}
-        onIndexChanged={(index) => setActiveIndex(index)}>
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
         {onboarding.map((item) => (
           <View key={item.id} className="flex items-center justify-center p-5">
-            <Image source={item.image} className="h-[300px] w-full" resizeMode="contain" />
-            <View className="mt-10 flex w-full flex-row items-center justify-center">
-              <Text className="mx-10 text-center text-3xl font-bold text-black">{item.title}</Text>
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
             </View>
-            <Text className="text-md font-JakartaSemiBold mx-10 mt-3 text-center text-[#858585]">
+            <Text className="text-md font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
               {item.description}
             </Text>
           </View>
@@ -43,11 +55,13 @@ const Home = () => {
       </Swiper>
 
       <CustomButton
-        title={isLastSlide ? 'Get Started' : 'Next'}
+        title={isLastSlide ? "Get Started" : "Next"}
         onPress={() =>
-          isLastSlide ? router.replace('/(auth)/sign-up') : swiperRef.current?.scrollBy(1)
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
         }
-        className="mb-5 mt-10 w-11/12"
+        className="w-11/12 mt-10 mb-5"
       />
     </SafeAreaView>
   );
