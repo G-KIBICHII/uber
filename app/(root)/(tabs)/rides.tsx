6a82f2732d5630333ab96493c61d5c53efac88eb
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-12-10 03:22:00 +0300

    message

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable import/order */
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const rides = () => {
+  return (
+    <View>
+      <Text>rides</Text>
+    </View>
+  )
+}
+
+export default rides
