4173f60d01cbd0707357eafcadab5a2699e96d7e
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-12-10 03:06:41 +0300

    message

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,49 @@
-/* eslint-disable prettier/prettier */
-import '../global.css';
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { LogBox } from "react-native";
 
-import { Stack } from 'expo-router';
 
-export default function Layout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{headerShown:false}} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env",
   );
 }
- 
+
+LogBox.ignoreLogs(["Clerk:"]);
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide(Async);
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+  
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+  );
+}
