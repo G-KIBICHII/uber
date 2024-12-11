c35b388c9b03fc5946538db118e605cb8a14ab72
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-12-11 12:10:11 +0300

    message

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Redirect } from 'expo-router'
+// import { useAuth } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
 
-const Home = () => {
-  return <Redirect href="/(auth)/welcome"/>
-}
+const Page = () => {
+  // const { isSignedIn } = useAuth();
 
-export default Home
+  // if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+
+  return <Redirect href="/(auth)/welcome" />;
+};
+
+export default Page;
